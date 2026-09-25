1a2dcba77da1dab0e864394e97a1de256e279a47
isaacceopixelart-collab <isaacceopixelart@gmail.com>
2026-09-25 15:58:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,8 @@ app.get("/google/refresh", async (req, res) => {
         });
     }
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
